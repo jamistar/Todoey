56a2c0aff25b4d3bd2a6254e5e4dc7d14020f9e6
Jami Lu <jamistar@icloud.com>
2018-02-06 18:41:54 +0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Jami on 2018/2/6.
+//  Copyright © 2018年 Jami. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
